327ddf0646a2da07554e925a2fa2066814e60f45
Ketan Sethi <ketansethi786@gmail.com>
2018-06-22 16:08:37 +0530

    test

--- src/test/java/com/demo/DemoProject/ApachePOIDemo.java
+++ src/test/java/com/demo/DemoProject/ApachePOIDemo.java
@@ -6,6 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
@@ -22,10 +25,11 @@ public class ApachePOIDemo {
 	XSSFWorkbook wb;
 	XSSFSheet sh;
 	File src;
+	private static Logger logger = LogManager.getLogger(ApachePOIDemo.class);
 
 	@BeforeMethod
 	public void setUp() throws IOException {
-
+		PropertyConfigurator.configure("log4j.properties");
 		src = new File("./data/file.xlsx");
 		fis = new FileInputStream(src);
 		wb = new XSSFWorkbook(fis);
@@ -38,21 +42,18 @@ public class ApachePOIDemo {
 	@Test
 	public void testPOI() {
 		for (int i = 1; i <= sh.getLastRowNum(); i++) {
+			logger.info(i);
 			driver.get(sh.getRow(i).getCell(0).getStringCellValue());
 			driver.manage().window().maximize();
 			driver.findElement(By.cssSelector("#lst-ib")).sendKeys(sh.getRow(i).getCell(1).getStringCellValue());
 			driver.findElement(By.name("btnK")).click();
 			sh.getRow(i).createCell(3).setCellValue(driver.getTitle());
+			logger.info(driver.getTitle());
 			if (driver.getTitle().equals(sh.getRow(i).getCell(2).getStringCellValue())) {
 				sh.getRow(i).createCell(4).setCellValue("PASS");
 			} else {
 				sh.getRow(i).createCell(4).setCellValue("FAIL");
 			}
-		}
-
-		for (int i = 1; i <= sh.getLastRowNum(); i++) {
-			driver.get(sh.getRow(i).getCell(0).getStringCellValue());
-
 		}
 
 	}
--- src/test/java/com/demo/DemoProject/Log4jDemo.java
+++ src/test/java/com/demo/DemoProject/Log4jDemo.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,7 +20,7 @@ public class Log4jDemo {
 
 	@BeforeMethod
 	public void setUp() throws IOException {
-
+		PropertyConfigurator.configure("log4j.properties");
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
