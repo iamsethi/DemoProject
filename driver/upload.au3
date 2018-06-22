752e4d4221775090a1219d9db3ec2daa46684497
Ketan Sethi <ketansethi786@gmail.com>
2018-06-22 15:14:31 +0530

    test

---
+++ driver/upload.au3
@@ -0,0 +1,3 @@
+ControlFocus("Open","","DirectUIHWND2")
+ControlSetText("Open","","DirectUIHWND2","C:\Users\ketan.sethi\Downloads\Doubts.txt")
+ControlClick("Open","","Button1")
--- src/test/java/com/demo/DemoProject/AutoITDemo.java
+++ src/test/java/com/demo/DemoProject/AutoITDemo.java
@@ -1,6 +1,12 @@
 package com.demo.DemoProject;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -25,7 +31,7 @@ public class AutoITDemo {
 
 		driver.get("file:///C:/Users/ketan.sethi/eclipse-workspace/DemoProject/data/fileupload.html");
 		driver.findElement(By.id("1")).click();
-		Runtime.getRuntime().exec("blogUpload.exe");
+		Runtime.getRuntime().exec("C:/Users/ketan.sethi/Desktop/upload.exe");
 		// https://www.autoitscript.com/site/autoit/downloads/
 		// First two Download Auto IT and Download Editor
 		// Go to C->Program Files -> AutoIT
@@ -46,7 +52,9 @@ public class AutoITDemo {
 	}
 
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown() throws IOException {
+		File f = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(f, new File("C:/Users/ketan.sethi/Desktop/snapshot.png"));
 		driver.close();
 	}
 }
