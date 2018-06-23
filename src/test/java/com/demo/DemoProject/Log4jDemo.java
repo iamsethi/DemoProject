cbd231bc15adf9c8c45904727e17a41d4cbdbe01
Ketan Sethi <ketansethi786@gmail.com>
2018-06-23 14:19:55 +0530

    log4j.properties and log4j.xml

--- src/test/java/com/demo/DemoProject/Log4jDemo.java
+++ src/test/java/com/demo/DemoProject/Log4jDemo.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,7 +19,7 @@ public class Log4jDemo {
 
 	@BeforeMethod
 	public void setUp() throws IOException {
-		PropertyConfigurator.configure("log4j.properties");
+		// PropertyConfigurator.configure("./src/main/resources/console-file-log4j.xml");
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
